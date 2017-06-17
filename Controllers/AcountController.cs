ebf9c190497c6796df8212f35dd24d923e32bf47
corn31295 <ngotu31295@gmail.com>
2017-06-17 13:03:28 +0700

    Log-in

---
+++ Controllers/AcountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace T2P_Project.Controllers
+{
+    public class AcountController : Controller
+    {
+        //
+        // GET: /Acount/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+	}
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="T2P_Project.MvcApplication" Language="C#" %>
